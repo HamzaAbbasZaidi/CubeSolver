987d16c87d7c212bede8d0cd24e1f0759911b3cf
HamzaAbbasZaidi <111506376+HamzaAbbasZaidi@users.noreply.github.com>
2022-08-21 03:49:35 +0530

    Add files via upload

--- CubeSolver/src/CubeSolver/Cube.java
+++ CubeSolver/src/CubeSolver/Cube.java
@@ -12,6 +12,8 @@ public class Cube {
 
     public static final ArrayList<String> allEdgeStickers = new ArrayList<>(Arrays.asList("UB", "UR", "UF", "UL", "LU", "LF", "LD", "LB", "FU", "FR", "FD", "FL", "RU", "RB", "RD", "RF", "BU", "BL", "BD", "BR", "DF", "DR", "DB", "DL"));
     public static final ArrayList<String> wxvuTries = new ArrayList<>(Arrays.asList("wxvu","Wxvu","wXvu","WXvu","wxVu","WxVu","wXVu","WXVu","wxvU","WxvU","wXvU","WXvU","wxVU","WxVU","wXVU","WXVU"));
+    public static final ArrayList<String> wxuvTries = new ArrayList<>(Arrays.asList("wxuv","Wxuv","wXuv","WXuv","wxUv","WxUv","wXUv","WXUv","wxuV","WxuV","wXuV","WXuV","wxUV","WxUV","wXUV","WXUV"));
+    public static final ArrayList<String> wvuxTries = new ArrayList<>(Arrays.asList("wvux","Wvux","wVux","WVux","wvUx","WvUx","wVUx","WVUx","wvuX","WvuX","wVuX","WVuX","wvUX","WvUX","wVUX","WVUX"));
     public Cube() {
 
         this.edges = new String[] {"UB","UR","UF","UL","FR","FL","BL","BR","DF","DR","DB","DL"};
@@ -39,6 +41,7 @@ public class Cube {
             Cube cubeCopy = new Cube(this.edges.clone(),this.corners.clone());
 
             for(int j = 0; j < 4; j++) {
+
                 switch (wxvuTries.get(i).charAt(j)) {
 
                     case 'w' -> cubeCopy.performMoves(CrossSolutions.edge1w1[allEdgeStickers.indexOf(cubeCopy.edges[10])], true, solutionBuilder);
@@ -59,20 +62,84 @@ public class Cube {
                 length = cancel(Arrays.asList(solutionBuilder.toString().split(" "))).size();
 
             }
+
+            System.out.println(cancel(Arrays.asList(solutionBuilder.toString().split(" "))));
+
+            cubeCopy = new Cube(this.edges.clone(),this.corners.clone());
+            solutionBuilder = new StringBuilder();
+
+            for(int j = 0; j < 4; j++) {
+
+                switch (wxuvTries.get(i).charAt(j)) {
+
+                    case 'w' -> cubeCopy.performMoves(CrossSolutions.edge1w1[allEdgeStickers.indexOf(cubeCopy.edges[10])], true, solutionBuilder);
+                    case 'W' -> cubeCopy.performMoves(CrossSolutions.edge1w2[allEdgeStickers.indexOf(cubeCopy.edges[10])], true, solutionBuilder);
+                    case 'x' -> cubeCopy.performMoves(CrossSolutions.edge2xw1[allEdgeStickers.indexOf(cubeCopy.edges[11])], true, solutionBuilder);
+                    case 'X' -> cubeCopy.performMoves(CrossSolutions.edge2xw2[allEdgeStickers.indexOf(cubeCopy.edges[11])], true, solutionBuilder);
+                    case 'u' -> cubeCopy.performMoves(CrossSolutions.edge3uwx1[allEdgeStickers.indexOf(cubeCopy.edges[8])], true, solutionBuilder);
+                    case 'U' -> cubeCopy.performMoves(CrossSolutions.edge3uwx2[allEdgeStickers.indexOf(cubeCopy.edges[8])], true, solutionBuilder);
+                    case 'v' -> cubeCopy.performMoves(CrossSolutions.edge4vuxw1[allEdgeStickers.indexOf(cubeCopy.edges[9])], true, solutionBuilder);
+                    case 'V' -> cubeCopy.performMoves(CrossSolutions.edge4vuxw2[allEdgeStickers.indexOf(cubeCopy.edges[9])], true, solutionBuilder);
+
+                }
+            }
+
+            if(cancel(Arrays.asList(solutionBuilder.toString().split(" "))).size() < length) {
+
+                finalCrossSolution = new StringBuilder(solutionBuilder.toString());
+                length = cancel(Arrays.asList(solutionBuilder.toString().split(" "))).size();
+
+            }
+
+            System.out.println(cancel(Arrays.asList(solutionBuilder.toString().split(" "))));
+
+            cubeCopy = new Cube(this.edges.clone(),this.corners.clone());
+            solutionBuilder = new StringBuilder();
+
+            for(int j = 0; j < 4; j++) {
+
+                switch (wvuxTries.get(i).charAt(j)) {
+
+                    case 'w' -> cubeCopy.performMoves(CrossSolutions.edge1w1[allEdgeStickers.indexOf(cubeCopy.edges[10])], true, solutionBuilder);
+                    case 'W' -> cubeCopy.performMoves(CrossSolutions.edge1w2[allEdgeStickers.indexOf(cubeCopy.edges[10])], true, solutionBuilder);
+                    case 'v' -> cubeCopy.performMoves(CrossSolutions.edge2vw1[allEdgeStickers.indexOf(cubeCopy.edges[9])], true, solutionBuilder);
+                    case 'V' -> cubeCopy.performMoves(CrossSolutions.edge2vw2[allEdgeStickers.indexOf(cubeCopy.edges[9])], true, solutionBuilder);
+                    case 'u' -> cubeCopy.performMoves(CrossSolutions.edge3uvw1[allEdgeStickers.indexOf(cubeCopy.edges[8])], true, solutionBuilder);
+                    case 'U' -> cubeCopy.performMoves(CrossSolutions.edge3uvw2[allEdgeStickers.indexOf(cubeCopy.edges[8])], true, solutionBuilder);
+                    case 'x' -> cubeCopy.performMoves(CrossSolutions.edge4x1[allEdgeStickers.indexOf(cubeCopy.edges[11])], true, solutionBuilder);
+                    case 'X' -> cubeCopy.performMoves(CrossSolutions.edge4x2[allEdgeStickers.indexOf(cubeCopy.edges[11])], true, solutionBuilder);
+
+                }
+            }
+            if(cancel(Arrays.asList(solutionBuilder.toString().split(" "))).size() < length) {
+
+                finalCrossSolution = new StringBuilder(solutionBuilder.toString());
+                length = cancel(Arrays.asList(solutionBuilder.toString().split(" "))).size();
+
+            }
+
+            System.out.println(cancel(Arrays.asList(solutionBuilder.toString().split(" "))));
         }
 
+        System.out.println("The shortest cross solution found is:");
         printMoves(cancel(Arrays.asList(finalCrossSolution.toString().split(" "))));
-
     }
 
     private void printMoves(List<String> listOfMoves) {
-        for(int i = 0; i < listOfMoves.size(); i++) System.out.print(listOfMoves.get(i).concat(" "));
+
+        for (String listOfMove : listOfMoves) System.out.print(listOfMove.concat(" "));
+
     }
     private List<String> cancel(List<String> sb) {
+
         while(isNotSimplified(sb) && sb.size() >= 1) {
+
             sb = makeFirstSimplificationPossible(sb);
+
         }
+
         return sb;
+
     }
 
     private boolean isNotSimplified(List<String> sb) {
@@ -109,26 +176,25 @@ public class Cube {
                     rotation -= 1;
                 }
 
-                switch(rotation) {
-                    case 0:
-                    case 4:
+                switch (rotation) {
+                    case 0, 4 -> {
                         sb.set(i, "");
                         sb.set(i + 1, "");
-                        break;
-                    case 1:
+                    }
+                    case 1 -> {
                         sb.set(i, Character.toString(sb.get(i).charAt(0)));
                         sb.set(i + 1, "");
-                        break;
-                    case -2:
-                    case 2:
+                    }
+                    case -2, 2 -> {
                         sb.set(i, Character.toString(sb.get(i).charAt(0)).concat("2"));
                         sb.set(i + 1, "");
-                        break;
-                    case 3:
+                    }
+                    case 3 -> {
                         sb.set(i, Character.toString(sb.get(i).charAt(0)).concat("'"));
                         sb.set(i + 1, "");
-                        break;
-                    default:
+                    }
+                    default -> {
+                    }
                 }
                 filtered = sb.stream().filter(k -> k != "").collect(Collectors.toList());
             break;
@@ -165,103 +231,81 @@ public class Cube {
 
     public void makeMove(String move, Cube output) {
 
-        switch(move) {
-
-            case "R":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.rMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.rMap.get(output.corners[i]);
-                break;
-
-            case "R'":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.rPrimeMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.rPrimeMap.get(output.corners[i]);
-                break;
-
-            case "R2":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.r2Map.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.r2Map.get(output.corners[i]);
-                break;
-
-            case "U":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.uMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.uMap.get(output.corners[i]);
-                break;
-
-            case "U'":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.uPrimeMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.uPrimeMap.get(output.corners[i]);
-                break;
-
-            case "U2":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.u2Map.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.u2Map.get(output.corners[i]);
-                break;
-
-            case "F":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.fMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.fMap.get(output.corners[i]);
-                break;
-
-            case "F'":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.fPrimeMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.fPrimeMap.get(output.corners[i]);
-                break;
-
-            case "F2":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.f2Map.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.f2Map.get(output.corners[i]);
-                break;
-
-            case "L":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.lMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.lMap.get(output.corners[i]);
-                break;
-
-            case "L'":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.lPrimeMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.lPrimeMap.get(output.corners[i]);
-                break;
-
-            case "L2":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.l2Map.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.l2Map.get(output.corners[i]);
-                break;
-
-            case "D":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.dMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.dMap.get(output.corners[i]);
-                break;
-
-            case "D'":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.dPrimeMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.dPrimeMap.get(output.corners[i]);
-                break;
-
-            case "D2":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.d2Map.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.d2Map.get(output.corners[i]);
-                break;
-
-            case "B":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.bMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.bMap.get(output.corners[i]);
-                break;
-
-            case "B'":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.bPrimeMap.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.bPrimeMap.get(output.corners[i]);
-                break;
-
-            case "B2":
-                for(int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.b2Map.get(output.edges[i]);
-                for(int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.b2Map.get(output.corners[i]);
-                break;
-
-            default:
-                throw new IllegalArgumentException(move+" is not a move");
+        switch (move) {
+            case "R" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.rMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.rMap.get(output.corners[i]);
+            }
+            case "R'" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.rPrimeMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.rPrimeMap.get(output.corners[i]);
+            }
+            case "R2" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.r2Map.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.r2Map.get(output.corners[i]);
+            }
+            case "U" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.uMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.uMap.get(output.corners[i]);
+            }
+            case "U'" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.uPrimeMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.uPrimeMap.get(output.corners[i]);
+            }
+            case "U2" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.u2Map.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.u2Map.get(output.corners[i]);
+            }
+            case "F" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.fMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.fMap.get(output.corners[i]);
+            }
+            case "F'" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.fPrimeMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.fPrimeMap.get(output.corners[i]);
+            }
+            case "F2" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.f2Map.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.f2Map.get(output.corners[i]);
+            }
+            case "L" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.lMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.lMap.get(output.corners[i]);
+            }
+            case "L'" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.lPrimeMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.lPrimeMap.get(output.corners[i]);
+            }
+            case "L2" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.l2Map.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.l2Map.get(output.corners[i]);
+            }
+            case "D" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.dMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.dMap.get(output.corners[i]);
+            }
+            case "D'" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.dPrimeMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.dPrimeMap.get(output.corners[i]);
+            }
+            case "D2" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.d2Map.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.d2Map.get(output.corners[i]);
+            }
+            case "B" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.bMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.bMap.get(output.corners[i]);
+            }
+            case "B'" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.bPrimeMap.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.bPrimeMap.get(output.corners[i]);
+            }
+            case "B2" -> {
+                for (int i = 0; i <= 11; i++) output.edges[i] = MoveMaps.b2Map.get(output.edges[i]);
+                for (int i = 0; i <= 7; i++) output.corners[i] = MoveMaps.b2Map.get(output.corners[i]);
+            }
+            default -> throw new IllegalArgumentException(move + " is not a move");
         }
-
     }
-
 }
 
